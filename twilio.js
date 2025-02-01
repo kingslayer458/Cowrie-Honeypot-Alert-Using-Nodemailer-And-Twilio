ec74a59b1f6e9c6dbffa3c3398c9225494f15e9a
kinglsayer4581 <148754404+kingslayer458@users.noreply.github.com>
2025-02-01 15:48:38 +0530

    Update twilio.js

--- twilio.js
+++ twilio.js
@@ -1,15 +1,15 @@
 const twilio = require('twilio');
 
 // Twilio SMS Configuration
-const twilioClient = twilio('AC8d2f09b199b9ab2224cf3bd5d48c5cfc', 'c78db7e3d9729a45b8ff331855e67840');
+const twilioClient = twilio('', '');//set your own secret key
 
 function sendTestSMS() {
     console.log("Attempting to send SMS...");
 
     twilioClient.messages.create({
         body: 'Test SMS from Twilio',
-        from: '+12185208337', // Replace with your Twilio phone number
-        to: '+919176012603' // Replace with the recipient's phone number, including country code (+91 for India)
+        from: '', // Replace with your Twilio phone number
+        to: '' // Replace with the recipient's phone number, including country code (+91 for India)
     })
     .then((message) => {
         console.log('SMS sent successfully.');
